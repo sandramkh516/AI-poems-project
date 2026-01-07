d7cfb5ebc414d4cb575fdbd5c1ebea96a962e8aa
sandramkh516 <118845782+sandramkh516@users.noreply.github.com>
2026-01-07 22:52:04 +0330

    improve the user experience

--- src/index.js
+++ src/index.js
@@ -7,21 +7,24 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
   let instructionInput = document.querySelector("#instructions");
+  poemElement.innerHTML = `<div class="generating" dir="ltr">⏳Generating a Persian poem about ${instructionInput.value}</div>`;
   let apiKey = "2cf310d7ae5ba0tbo139ab4b634a8bf3";
-  let context = `You are an expert in classical Persian poetry.The user will enter a single Persian word.Choose ONE well-known Iranian poet (such as Hafez, Saadi, Rumi, Ferdowsi, or Khayyam).Generate exactly FOUR complete Persian verses (beyts) from that
-  poet that contain the given word explicitly and naturally.
-
+  let context = `You are an expert in classical Persian poetry.The user will enter a single English word.Choose ONE well-known Iranian poet (such as Hafez, Saadi, Rumi, Ferdowsi, or Khayyam).
+  Generate exactly FOUR complete Persian verses (beyts) from that
+  poet that contain the given word explicitly and naturally.Seperate each verse using the HTML line break tag.
 Rules:
  The verses must be in Persian (Farsi). All four verses must include the exact given word.
- Verses should be authentic in style and language of the selected poet.Seperate each line with a <br/>
-  Do NOT explain anything.
+ Verses should be authentic in style and language of the selected poet.
+ Do NOT explain anything.
  Do NOT add titles, translations, or extra text.
  Output only the four verses, each on a separate line.`;
 
   let prompt = `user instructions:generate a persian poem about ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log(apiUrl);
+  console.log(prompt);
   axios.get(apiUrl).then(displayPoem);
 }
 
