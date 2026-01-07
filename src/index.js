5a292640d37ff26a8f34a05134c712231fe3226e
sandramkh516 <118845782+sandramkh516@users.noreply.github.com>
2026-01-07 18:28:35 +0330

    added basic form handing and added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "بر خیر و مخور غم جهان گذرا",
+    autoStart: true,
+  });
+}
 
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
